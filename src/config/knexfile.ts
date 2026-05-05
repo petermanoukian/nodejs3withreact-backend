7ef8ab0272a759f1db7540e1b8a9b6d2933e2b20
Peter Manoukian <thewizard_it@hotmail.com>
2026-05-05 16:49:10 +0400

    Version 16 - DB Configurations

--- src/config/knexfile.ts
+++ src/config/knexfile.ts
@@ -1,17 +1,26 @@
 // src/config/knexfile.ts
-import "./env"; // must be first
+
+import "./env";
 import path from "path";
 import { config } from "./index";
 
 export default {
   client: "mysql2",
+
   connection: {
     host: config.db.host,
     port: config.db.port,
     user: config.db.user,
     password: config.db.password,
     database: config.db.database,
+
+    ssl: {
+      ca: process.env.DB_CA_CERT?.replace(/\\n/g, "\n"),
+      rejectUnauthorized: true,
+      minVersion: "TLSv1.2",
+    },
   },
+
   migrations: {
     directory: path.resolve(__dirname, "../database/migrations"),
     extension: "ts",
